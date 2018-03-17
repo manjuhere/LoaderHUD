410a7f5de50578a1fcef4f07eefb9896a927b366
Manjunath Chandrashekar <mbond127@gmail.com>
2018-03-17 11:48:07 +0530

    Initial Commit

---
+++ loaderHUD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  loaderHUD
+//
+//  Created by Manjunath Chandrashekar on 17/03/18.
+//  Copyright © 2018 Manjunath Chandrashekar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
